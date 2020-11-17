22410734e376b7a89f3d434c8f36eb7777caa6a1
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-11-16 16:35:29 -0800

    Update fishfarmAC.py

--- fishfarmAC.py
+++ fishfarmAC.py
@@ -1,18 +1,11 @@
-## Script made by KanuX for Fish Farm tutorial ##
-## Follow me: twitch.tv/kanux14 ##
-
 from pynput.mouse import Button, Controller
 import keyboard as kb
 import time
 
-wait = time.sleep
-ms = Controller()
-bool = False
-
 while True:
 	if kb.is_pressed('z'):
 		bool = not bool
-		wait(0.5)
+		time.sleep(0.5)
 		ms.click(Button.right, 1)
 	if bool == True:
 		ms.press(Button.right)
